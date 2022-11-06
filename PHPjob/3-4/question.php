66ed4ddfe058ded4c5d317ba6e2c0e95db217674
atom01210 <ayako.tomozawa@gmail.com>
2022-11-06 16:19:32 +0900

    Create question.php

---
+++ PHPjob/3-4/question.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>3章チェックテスト_question</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        //POST送信で送られてきた名前を受け取って変数を作成
+        $name = $_POST['name'];
+        
+        //①画像を参考に問題文の選択肢の配列を作成してください。
+        $question1_arry = ["80", "22", "20", "21"];
+        $question2_arry = ["PHP", "Python", "JAVA", "HTML"];
+        $question3_arry = ["join", "select", "insert", "update"];
+        
+        //② ①で作成した、配列から正解の選択肢の変数を作成してください
+        $correctAns1 = $question1_arry[0];
+        $correctAns2 = $question2_arry[3];
+        $correctAns3 = $question3_arry[1];
+    ?>
+
+    <p>お疲れ様です<?php echo $name; ?>さん</p>
+    <!--フォームの作成 通信はPOST通信で-->
+    <h2>①ネットワークのポート番号は何番？</h2>
+    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <form action="answer.php" method="post">
+        <?php
+        foreach ($question1_arry as $value1) { ?>
+            <input type="radio" name="Ans1" value="<?php echo $value1;?>">
+            <?php echo $value1;
+        }?>
+        <br>
+    </form>
+
+    <h2>②Webページを作成するための言語は？</h2>
+    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <form action="answer.php" method="post">
+        <?php
+        foreach ($question2_arry as $value2) { ?>
+            <input type="radio" name="Ans2" value="<?php echo $value2;?>">
+            <?php echo $value2;
+        }?>
+        <br>
+    </form>
+
+    <h2>③MySQLで情報を取得するためのコマンドは？</h2>
+    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <form action="answer.php" method="post">
+        <?php
+        foreach ($question3_arry as $value3) { ?>
+            <input type="radio" name="Ans3" value="<?php echo $value3;?>">
+            <?php echo $value3;
+        }?>
+        <br>
+    </form>
+
+    <!--<?php
+    foreach($question3_arry as $key => $value3) {
+        echo '<label><input type="radio" name="Ans3" value="'.  $key . '" />'  . $value3 . '</label>' . "\n";
+    }
+    echo '<br>';
+    ?> -->
+
+    <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
+    <form action="answer.php" method="post">
+        <input type="hidden" name="name" value="<?php echo $name; ?>" />
+        <input type="hidden" name="Ans1" value="<?php echo $value1; ?>" />
+        <input type="hidden" name="Ans2" value="<?php echo $value2; ?>" />
+        <input type="hidden" name="Ans3" value="<?php echo $value3; ?>" />
+        <input type="hidden" name="correctAns1" value="<?php echo $correctAns1; ?>" />
+        <input type="hidden" name="correctAns2" value="<?php echo $correctAns2; ?>" />
+        <input type="hidden" name="correctAns3" value="<?php echo $correctAns3; ?>" />
+        <input type="submit" value="回答する" /> 
+    </form>
+
+</body>
+</html>
