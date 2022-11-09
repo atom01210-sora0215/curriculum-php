b9a7522553200e5ab830f12c2146279965cdb23b
atom01210 <ayako.tomozawa@gmail.com>
2022-11-09 22:38:06 +0900

    課題提出テスト
    ７章チェックテスト

---
+++ PHPjob/7/main.js
@@ -0,0 +1,12 @@
+let numbers = [2, 5, 12, 13, 15, 18, 22];
+//ここに答えを実装してください。↓↓↓
+for (let i = 0; i < numbers.length; i++) {
+    let num = numbers[i];
+    if (num % 2 == 0){
+        function isEven() {
+            console.log(num + 'は偶数です');
+        } 
+    }
+    isEven();
+}
+
