35e3e6c22adf62a18bf00d9c3243d6f7d16fbe65
atom01210 <ayako.tomozawa@gmail.com>
2022-11-09 23:10:01 +0900

    課題提出テスト
    ７章チェックテスト

--- PHPjob/7/main.js
+++ PHPjob/7/main.js
@@ -1,3 +1,4 @@
+//問1
 let numbers = [2, 5, 12, 13, 15, 18, 22];
 //ここに答えを実装してください。↓↓↓
 for (let i = 0; i < numbers.length; i++) {
@@ -10,3 +11,15 @@ for (let i = 0; i < numbers.length; i++) {
     isEven();
 }
 
+//問2
+class car {
+    constructor (gas, number){
+        this.gas = gas;
+        this.number = number;
+    }
+    getNumGas(){
+        console.log(`ガソリンは${this.gas}です。ナンバーは${this.number}です`);
+    }
+}
+let example = new car(123,4567);
+example.getNumGas();
